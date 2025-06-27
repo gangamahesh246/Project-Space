36232d351502ba9dc4faa3bd482d28e6580aa982
gangamahesh246 <gangamahesh809@gmail.com>
2025-06-27 21:02:43 +0530

    build file

--- src/Home_page/Home.jsx
+++ src/Home_page/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Header from "./sections/Header";
 import Second_Section from "./sections/Second_Section";
-import FlowChart from "./sections/flow_chart";
+import FlowChart from "./sections/Flow_Chart";
 import Footer from "./sections/Footer";
 
 const Home = () => {
